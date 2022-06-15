d9fdeec3852273799a40cc08e7a0e1972c306e6b
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2022-06-15 19:07:25 +0100

    back to reading

--- chapter 2/main.js
+++ chapter 2/main.js
@@ -62,3 +62,55 @@ function chessboard(){
   console.log(hash)
 }
 chessboard()
+
+
+function greetDevelopers(list) {
+  // thank you for checking out my kata :)
+  let results = list.map((el) => {
+    const {firstName, lastName, country, continent, age, language} = el
+
+    return {
+      firstName, 
+      lastName, 
+      country, 
+      continent, 
+      age, 
+      language,
+       greeting: `Hi ${firstName}, what do you like the most about ${language}?`
+    }
+  })
+  
+  console.log(results)
+}
+
+//Write a function min that takes two arguments and returns their minimum.
+function minimum (a, b){
+  if(a < b){
+    console.log(a);
+  } else {
+    console.log(b);
+  }
+}
+
+
+function RecurisEvension () {
+    if (n == 0) return true;
+    else if (n == 1) return false;
+    else if (n < 0) return isEven(-n);
+    else return isEven(n - 2); 
+}
+function countChar(string, ch) {
+  let counted = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == ch) {
+      counted += 1;
+    }
+  }
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+countBs("BBs")
